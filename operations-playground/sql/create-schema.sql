2f90c62dd12834feda7b21a4249aaaa26b337356
aleksey.pershin <aperhin@it-one.ru>
2021-09-26 22:59:54 +0300

    initial

---
+++ operations-playground/sql/create-schema.sql
@@ -0,0 +1,8 @@
+CREATE TABLE CLICK_EVENT_STATISTICS
+(
+                             id SERIAL PRIMARY KEY,
+                             windowStart TIMESTAMP ,
+                             windowEnd TIMESTAMP,
+                             page VARCHAR ,
+                             count DECIMAL
+)
