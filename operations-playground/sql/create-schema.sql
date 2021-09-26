1e18956afcca2c62453032eece0d3c37813f59ca
alexpershin <aperhin@it-one.ru>
2021-09-26 23:34:35 +0300

    format

--- operations-playground/sql/create-schema.sql
+++ operations-playground/sql/create-schema.sql
@@ -1,8 +1,8 @@
 CREATE TABLE CLICK_EVENT_STATISTICS
 (
-                             id SERIAL PRIMARY KEY,
-                             windowStart TIMESTAMP ,
-                             windowEnd TIMESTAMP,
-                             page VARCHAR ,
-                             count DECIMAL
+    id          SERIAL PRIMARY KEY,
+    windowStart TIMESTAMP,
+    windowEnd   TIMESTAMP,
+    page        VARCHAR,
+    count       DECIMAL
 )
